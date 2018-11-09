a704a37d6a74088987791f3c1943e2760e7e3eef
William <715guoyong@gmail.com>
2018-11-08 22:55:25 -0800

    update main

--- src/root/MainTest.java
+++ src/root/MainTest.java
@@ -6,9 +6,8 @@ import java.util.List;
 public class MainTest {
 
 	public static void main(String[] args) throws IOException {
-		// String url = "src/rootTest/input.txt";
+		// String url = "src/root/input.txt";
 		DriverTrip dt = new DriverTrip();
-		System.out.println(args[0]);
 		List<String> inputList = dt.readFile(args[0]);
 		dt.splitLines(inputList);
 		dt.outputRes();
